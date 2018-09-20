7fbaeff014e26ad08895bad0c92eee6182c4c090
Silot <aposnaml@teiser.gr>
2018-09-20 19:31:02 +0300

    player vs bot | player vs player | game status| score | physics

--- src/pong/Paddle.java
+++ src/pong/Paddle.java
@@ -1,33 +1,66 @@
-
 package pong;
 
-/**
- *
- * @author tolis
- */
-public class Paddle {
-    
-    public int paddleNumber;
-    
-    public int x, y, width = 100, height = 500;
-    
-    public int score;
-    
-    public Paddle(int paddleNumber){
-        this.paddleNumber = paddleNumber;
-        
-        if(paddleNumber == 1){
-            this.x = width;
-        }
-        if(paddleNumber == 2){
-            this.x = Pong.pong.width - width;
-            
-        }
-        
-         this.y = Pong.pong.height/2 - this.height/2;
-        
-        
-    }
-    
-    
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Paddle
+{
+
+	public int paddleNumber;
+
+	public int x, y, width = 50, height = 250;
+
+	public int score;
+
+	public Paddle(Pong pong, int paddleNumber)
+	{
+		this.paddleNumber = paddleNumber;
+
+		if (paddleNumber == 1)
+		{
+			this.x = 0;
+		}
+
+		if (paddleNumber == 2)
+		{
+			this.x = pong.width - width;
+		}
+
+		this.y = pong.height / 2 - this.height / 2;
+	}
+
+	public void render(Graphics g)
+	{
+		g.setColor(Color.WHITE);
+		g.fillRect(x, y, width, height);
+	}
+
+	public void move(boolean up)
+	{
+		int speed = 15;
+
+		if (up)
+		{
+			if (y - speed > 0)
+			{
+				y -= speed;
+			}
+			else
+			{
+				y = 0;
+			}
+		}
+		else
+		{
+			if (y + height + speed < Pong.pong.height)
+			{
+				y += speed;
+			}
+			else
+			{
+				y = Pong.pong.height - height;
+			}
+		}
+	}
+
 }
